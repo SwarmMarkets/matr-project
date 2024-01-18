6f57c4816e17453f947f639490dc295fdb47c2d8
Bohdan <pukhnobg@gmail.com>
2024-01-18 12:17:03 +0200

    configured: config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -32,53 +32,52 @@ const config: HardhatUserConfig = {
     ],
   },
   defaultNetwork: 'hardhat',
-  // TODO: uncomment for the deployment
-  // networks: {
-  //   hardhat: {
-  //     blockGasLimit: 20000000,
-  //     throwOnCallFailures: false,
-  //     chainId: 31337,
-  //     initialBaseFeePerGas: 0,
-  //     accounts: {
-  //       mnemonic,
-  //       accountsBalance: "10000000000000000000000000",
-  //     },
-  //     forking: {
-  //       url: `https://eth-mainnet.alchemyapi.io/v2/${ethApiKey}`,
-  //       enabled: true,
-  //       //blockNumber: 16383055,
-  //     },
-  //   },
-  //   mainnet: {
-  //     url: `https://eth-mainnet.alchemyapi.io/v2/${ethApiKey}`,
-  //     chainId: 1,
-  //     accounts: [privateKey],
-  //     gas: 2100000,
-  //     gasPrice: 60000000000, // 45
-  //   },
-  //   sepolia: {
-  //     url: `https://eth-sepolia.g.alchemy.com/v2/${sepoliaApiKey}`,
-  //     chainId: 11155111,
-  //     accounts: [privateKey],
-  //     gas: 2100000,
-  //     gasPrice: 45000000000, // 45
-  //   },
-  //   polygon: {
-  //     url: `https://polygon-mainnet.g.alchemy.com/v2/${polygonApiKey}`,
-  //     chainId: 137,
-  //     accounts: [privateKey],
-  //     gas: 5000000,
-  //     gasPrice: 250000000000, // 250
-  //   },
-  //   mumbai: {
-  //     url: `https://polygon-mumbai.g.alchemy.com/v2/${mumbaiApiKey}`,
-  //     chainId: 80001,
-  //     accounts: [privateKey],
-  //     gas: 2100000,
-  //     gasPrice: 45000000000, // 45
-  //     gasMultiplier: 2,
-  //   },
-  // },
+  networks: {
+    hardhat: {
+      blockGasLimit: 20000000,
+      throwOnCallFailures: false,
+      chainId: 31337,
+      initialBaseFeePerGas: 0,
+      accounts: {
+        mnemonic,
+        accountsBalance: "10000000000000000000000000",
+      },
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ethApiKey}`,
+        enabled: true,
+        //blockNumber: 16383055,
+      },
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ethApiKey}`,
+      chainId: 1,
+      accounts: { mnemonic },
+      gas: 2100000,
+      gasPrice: 60000000000, // 45
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${sepoliaApiKey}`,
+      chainId: 11155111,
+      accounts: [privateKey],
+      gas: 2100000,
+      gasPrice: 45000000000, // 45
+    },
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${polygonApiKey}`,
+      chainId: 137,
+      accounts: { mnemonic },
+      gas: 5000000,
+      gasPrice: 250000000000, // 250
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${mumbaiApiKey}`,
+      chainId: 80001,
+      accounts: [privateKey],
+      gas: 2100000,
+      gasPrice: 45000000000, // 45
+      gasMultiplier: 2,
+    },
+  },
   gasReporter: {
     coinmarketcap: process.env.COIN_MARKET_CAP_KEY,
     currency: 'USD',
